4436150716c0d9b4764bcb44feb74f5220277801
Rahul9.Kamble <rahul9.kamble@ril.com>
2022-04-28 18:47:24 +0530

    Data Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,48 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 
-const DISHES : Dish[] = [
-  {
-    id: '0',
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-  },
-  {
-    id: '1',
-    name: 'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    featured: false,
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-  },
-  {
-    id: '2',
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    featured: false,
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-  },
-  {
-    id: '3',
-    name: 'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    featured: false,
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-  }
-];
+
 
 @Component({
   selector: 'app-menu',
@@ -51,13 +11,16 @@ const DISHES : Dish[] = [
 })
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
-
-  selectedDish : any = DISHES[0];
+  dishes : Dish[] = DISHES;
+  selectedDish : Dish | any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
   }
 
 }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,4 @@
+import { Comment } from './comment';
 
 export class Dish {
     id: string ="";
@@ -8,4 +9,5 @@ export class Dish {
     label: string ="";
     price: string ="";
     description: string ="";
+    comments: Comment[] = [];
 }
